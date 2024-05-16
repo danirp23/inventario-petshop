da26c1a7ebcd8ca93395c6991ea9cf20b63792f9
Daniela Rodriguez <>
2024-05-16 01:09:42 -0500

    feat: :sparkles: se agrega llamado al servicio

--- src/pages/StockProducts.jsx
+++ src/pages/StockProducts.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 export default function StockProducts() {
+  const [items, setItems] = useState([
+    
+  ]);
   return (
     <div>
         <h1>Listar Productos</h1>
+        <div className='table'>
+
+        </div>
     </div>
   )
 }
---
+++ src/services/axios.config.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const URL = "https://664579b1b8925626f891f7c4.mockapi.io/api/stockProducts";
+
+export const axiosInstance = axios.create({
+    baseURL: URL
+});
+
+export const createNewProduct = async (values) => {
+    try {
+        const response = await axiosInstance.post('/', values);
+        return response;
+    } catch (error) {
+        console.error("Error al crear el producto:", error);
+        throw error;
+    }
+};
